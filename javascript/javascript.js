e4273dfa2ead7ab2401eb58d7c25eb73ba46c55e
allysomoliveira7@gmail.com <62918698+Allysonubius@users.noreply.github.com>
2020-04-05 18:10:53 -0300

    Criado comando js hello para pagina index.html.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,5 +2,12 @@
  * JS - Vinculando um documento JS
  * @author Allyson de OLiveira Brandao
  * */
+/*hello*/
+function Hello() {
+    var nome;
+    nome = prompt("Qual é o seu nome ?");
+    alert(" Hello " + nome);
+}
+/*rodape*/
 document.write("<h3>" + "&copy; Allyson de Oliveira Brandão - Todos os direitos reservados." +
     "</h3>");
