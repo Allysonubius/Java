afa4a1283a2da33183330355cef0f755deb005f3
allysomoliveira7@gmail.com <62918698+Allysonubius@users.noreply.github.com>
2020-04-05 20:26:15 -0300

    Criado calculadora compensatoria de Entanol ou gasolina com JavaScript.

--- js/calcflex.js
+++ js/calcflex.js
@@ -3,3 +3,19 @@
  * @author Allyson de OLiveira Brandao
  * */
 /*calculadora Flex*/
+var etanol, gasolina;
+
+function calcular() {
+    /*ETANOL*/
+    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",", "."));
+    /*GASOLINA*/
+    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",", "."));
+    /*COMPARAÇÂO*/
+    if (etanol < 0.7 * gasolina) {
+        //verdadeiro
+        document.getElementById("status").src = "img/etanol.png";
+    } else {
+        //falso
+        document.getElementById("status").src = "img/gasoline.jpg";
+    }
+}
